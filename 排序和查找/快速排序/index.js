b8365e253d02d0d223072272a084b86e7bddc23d
zhulandashui <211824436@qq.com>
2022-09-18 18:22:10 +0800

    优化写法

--- 排序和查找/快速排序/index.js
+++ 排序和查找/快速排序/index.js
@@ -1,24 +1,47 @@
-let arr = [2,3,8,5,1,4,9,7,6]
+let arr = [2, 3, 8, 5, 1, 4, 9, 7, 6]
 
-function quickSort(arr){
+function quickSort(arr) {
     // 递归结束条件,数组里只有一个数组或者没有元素就直接返回
-    if(arr.length<=1) return arr
-    let mid = Math.floor(arr.length/2)
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length / 2)
     let left = []
     let right = []
-    let pivot = arr.splice(mid,1)[0]
-    
+    let pivot = arr.splice(mid, 1)[0]
+
     arr.forEach(item => {
-      // 比pivot大的放右边，小的放左边
-        if(item<=pivot){
-          left.push(item)
-        }else{
-          right.push(item)
+        // 比pivot大的放右边，小的放左边
+        if (item <= pivot) {
+            left.push(item)
+        } else {
+            right.push(item)
         }
     });
     //返回拼接数组
-    return quickSort(left).concat([pivot],quickSort(right))
+    return quickSort(left).concat([pivot], quickSort(right))
 }
 console.log(
-quickSort(arr)
+    quickSort(arr)
+);
+
+//写法优化
+function quickSort(arr) {
+    // 递归结束条件,数组里只有一个数组或者没有元素就直接返回
+    if (arr.length <= 1) return arr
+    let flag = arr[0]
+    let left = []
+    let right = []
+
+    for (let i = 1; i < arr.length; i++) {
+        // 比pivot大的放右边，小的放左边
+        if (arr[i] <= flag) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    //返回拼接数组
+    return quickSort(left).concat([flag], quickSort(right))
+}
+console.log(
+    quickSort(arr)
 );
