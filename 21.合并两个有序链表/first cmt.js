43ddcfd5bbc8142d1f0d5de94fa6e55113513990
zhulandashui <211824436@qq.com>
2022-09-21 23:54:04 +0800

    loading

---
+++ 21.合并两个有序链表/first cmt.js
@@ -0,0 +1,14 @@
+function merge(list1, list2) {
+    let i = list1,
+        j = list2
+    let head = {}
+
+    if (j >= i) {
+        head.next = j
+        j = j.next
+    } else {
+
+    }
+
+
+}
